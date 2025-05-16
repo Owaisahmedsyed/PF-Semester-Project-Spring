72fcb15b538aa34b28758940b46f5f276a657816
Owaisahmedsyed <sowaisahmed944@gmail.com>
2025-05-16 09:24:10 -0700

    By Group

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_APPOINTMENTS 100
+
+// Structure for Appointment
+struct Appointment {
+    int id;
+    char patientName[50];
+    char gender[10];           // New field
+    char doctorName[50];
+    char date[15];
+    char time[10];
+};
+
+// Function declarations
+void addAppointment(struct Appointment *appointments, int *count);
+void viewAppointments(struct Appointment *appointments, int count);
+void saveToFile(struct Appointment *appointments, int count);
+void loadFromFile(struct Appointment *appointments, int *count);
+void searchAppointment(struct Appointment *appointments, int count);
+void deleteAppointment(struct Appointment *appointments, int *count);
+void menu();
